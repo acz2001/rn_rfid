db8698e68d14a1632da36f10df5ef7bb6c331fe0
zhaokunzhao <406123980@qq.com>
2023-05-19 13:34:57 +0800

    test: 功能测试

--- android/app/src/main/java/com/rfid/ToastModule.java
+++ android/app/src/main/java/com/rfid/ToastModule.java
@@ -1,6 +1,7 @@
 package com.rfid;
 
 import android.widget.Toast;
+
 import java.util.logging.Logger;
 
 import com.facebook.react.bridge.Callback;
@@ -29,107 +30,131 @@ import com.solid.Gen2.InventryValue;
 
 public class ToastModule extends ReactContextBaseJavaModule {
 
-  private static final Logger LOGGER = Logger.getLogger(ToastModule.class.getName());
-  private static ReactApplicationContext reactContext;
+    private static final Logger LOGGER = Logger.getLogger(ToastModule.class.getName());
+    private static ReactApplicationContext reactContext;
 
-  private static final String DURATION_SHORT_KEY = "SHORT";
-  private static final String DURATION_LONG_KEY = "LONG";
+    private static final String DURATION_SHORT_KEY = "SHORT";
+    private static final String DURATION_LONG_KEY = "LONG";
 
-  public ToastModule(ReactApplicationContext context) {
-    super(context);
-    reactContext = context;
-  }
+    public ToastModule(ReactApplicationContext context) {
+        super(context);
+        reactContext = context;
+    }
 
-  @Override
-  public String getName() {
-    return "ToastExample";
-  }
+    @Override
+    public String getName() {
+        return "ToastExample";
+    }
 
-  @Override
-  public Map<String, Object> getConstants() {
-    final Map<String, Object> constants = new HashMap<>();
-    constants.put(DURATION_SHORT_KEY, Toast.LENGTH_SHORT);
-    constants.put(DURATION_LONG_KEY, Toast.LENGTH_LONG);
-    return constants;
-  }
+    @Override
+    public Map<String, Object> getConstants() {
+        final Map<String, Object> constants = new HashMap<>();
+        constants.put(DURATION_SHORT_KEY, Toast.LENGTH_SHORT);
+        constants.put(DURATION_LONG_KEY, Toast.LENGTH_LONG);
+        return constants;
+    }
 
-  @ReactMethod
-  public void show(String message, int duration, Callback errorCallback, @NotNull Callback successCallback) {
+    @ReactMethod
+    public void show(String message, int duration, Callback errorCallback, @NotNull Callback successCallback) {
 //    LOGGER.info("This is an info message.");
-    successCallback.invoke(message);
-  }
-
-  @ReactMethod
-  public void create(String uriString, Callback errorCallback, Callback successCallback) {
-    try
-    {
-      System.out.println("SReader");
-      SReader reader = SReader.create("sld:///dev/ttyS6");
-
-      reader.Connect();
-//
-//      int[] antList = {4};
-//      reader.setAntenna(antList);
-//
-//      int power = 30;
-//      reader.setReaderPower(power);
-//
-//      ReaderListener listener = new PrintListener();
-//      reader.addReadListener(listener);
-//
-//      int q= 4;
-//      int session = 1;
-//      Gen2.InventryValue value = new InventryValue(q, session);
-//
-//      reader.Inventry(value, null);
-//
-      successCallback.invoke(uriString);
-//
-//
-//      reader.ShutDown();
-    }
-    catch (ReaderException e) {
-      // TODO: handle exception
-      System.out.println("ReaderException:"+e.getMessage());
-    }
-    catch (Exception e) {
-      // TODO: handle exception
-      System.out.println("Exception"+e.getMessage());
-      errorCallback.invoke(e.getMessage());
+        successCallback.invoke(message);
     }
 
-  }
+    private static SReader reader;
 
-  static class PrintListener implements ReaderListener
-  {
-    HashSet<String> seenTags = new HashSet<String>();
-    int total = 1;
-
-    @Override
-    public void ReaderGPIO(Gpio_Info gpio_info) {
-      // TODO Auto-generated method stub
-      System.out.println("Id:"+gpio_info.getID()+"  High:"+gpio_info.getHigh() );
+    /**
+     * 断开连接
+     */
+    @ReactMethod
+    public void shutdown() {
+        if (reader != null) {
+            reader.ShutDown();
+        }
     }
 
-    @Override
-    public void ReaderException(Exception e) {
-      // TODO Auto-generated method stub
-      System.out.println("ee ReaderException: " + e.getMessage());
+    /**
+     * 结束盘点
+     */
+    @ReactMethod
+    public void inventryStop() {
+        if (reader != null) {
+            reader.Inventry_stop();
+        }
     }
 
-    @Override
-    public void TagReadData(TagData t) {
-      // TODO Auto-generated method stub
-      String epc = t.epcString();
-      //System.out.println("New tag: " + t.epcString()+" ant:"+ t.getAnt());
-      if (!seenTags.contains(epc))
-      {
-        System.out.println("New tag: " + t.epcString()+" ant:"+ t.getAnt()+ "  total:"+total);
-        total++;
-        seenTags.add(epc);
-      }
+    @ReactMethod
+    public void create(String uriString, Callback errorCallback, Callback successCallback) {
+        try {
+            reader = SReader.create("sld://" + uriString);
+
+            reader.Connect();
+
+            // 设置天线
+            int[] antList = {1};
+            reader.setAntenna(antList);
+
+            // 设置功率
+            int power = 30;
+            reader.setReaderPower(power);
+
+            ReaderListener listener = new PrintListener();
+            reader.addReadListener(listener);
+
+            int q = 4;
+            int session = 1;
+            Gen2.InventryValue value = new InventryValue(q, session);
+            reader.Inventry(value, null);
+
+
+            successCallback.invoke(this.ReadSingleEPC());
+        } catch (ReaderException e) {
+            // TODO: handle exception
+            System.out.println("ReaderException:" + e.getMessage());
+            errorCallback.invoke(e.getMessage());
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("Exception" + e.getMessage());
+            errorCallback.invoke(e.getMessage());
+        }
+
     }
 
-  }
+    /**
+     * 读取EPC
+     */
+    private String ReadSingleEPC() {
+        TagData tagData = reader.ReadSingleEPC();
+        return tagData.epcString()
+    }
+
+    static class PrintListener implements ReaderListener {
+        HashSet<String> seenTags = new HashSet<String>();
+        int total = 1;
+
+        @Override
+        public void ReaderGPIO(Gpio_Info gpio_info) {
+            // TODO Auto-generated method stub
+            System.out.println("Id:" + gpio_info.getID() + "  High:" + gpio_info.getHigh());
+        }
+
+        @Override
+        public void ReaderException(Exception e) {
+            // TODO Auto-generated method stub
+            System.out.println("ee ReaderException: " + e.getMessage());
+        }
+
+        @Override
+        public void TagReadData(TagData t) {
+            // TODO Auto-generated method stub
+            String epc = t.epcString();
+            // System.out.println("New tag: " + t.epcString()+" ant:"+ t.getAnt());
+            if (!seenTags.contains(epc)) {
+                System.out.println("New tag: " + t.epcString() + " ant:" + t.getAnt() + "  total:" + total);
+                total++;
+                seenTags.add(epc);
+            }
+        }
+
+    }
 
 }
